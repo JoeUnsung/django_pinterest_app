f1b9a144b521d076ebaf61e872b37433d4718068
Joe <esj1020@gmail.com>
2021-07-08 23:31:46 +0900

    feat: User 생성

--- acountapp/models.py
+++ acountapp/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Content(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     text = models.CharField(max_length=255, null=False)
+    # user_id = models.ForeignKey(to_field=id, db_column=)
+
+# class UserAccount(models.Model):
+#     id = models.CharField(max_length= 100, primary_key=True)
+#     name = models.CharField(max_length=200, null=False)
