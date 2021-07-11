79a0ca858b3f671be7160c5b22bfbdd13636687e
Joe <esj1020@gmail.com>
2021-07-11 22:06:48 +0900

    fix: 버그 픽스

--- pinterest_django/urls.py
+++ pinterest_django/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('account/', include('acountapp.urls')),
+    path('accounts/', include('acountapp.urls')),
 ]
