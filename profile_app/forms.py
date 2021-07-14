9431e39fa2dc27a5367e9ec12bd20bc29a39e0ff
Joe <esj1020@gmail.com>
2021-07-14 22:52:30 +0900

    feat: Profile Form화

---
+++ profile_app/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+
+from profile_app.models import Profile
+
+class ProfileCreationForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'image',
+            'nickname',
+            'message',
+        ]
