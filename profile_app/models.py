0d0c9286f0236aeba645708a4c598cddf422ee43
Joe <esj1020@gmail.com>
2021-07-14 23:54:59 +0900

    feat: Profile 앱 생성

--- profile_app/models.py
+++ profile_app/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Profile(models.model):
-    user_id = models.OneToOneField(User,
+class Profile(models.Model):
+    user = models.OneToOneField(User,
                                    on_delete=models.CASCADE, # User가 사라질 때 어떻게 되는지를 설정하는 부분
                                    related_name='profile') # request.User.profile.nickname과 같이 마더 테이블에서 부터 접근이 가능
     image = models.ImageField(upload_to='profile/', null=True)
@@ -11,5 +11,3 @@ class Profile(models.model):
                                 unique=True, # 유일 조건
                                 null=True)
     message = models.CharField(max_length=150, null=True)
-
-
--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,9 +1,12 @@
-
 from django.urls import path
+
+from profile_app.views import ProfileCreateView
 
 
 app_name = 'profile_app'
 
 urlpatterns = [
-    path()
+    path('make/', ProfileCreateView.as_view(), name='make_profile'),
+
+
 ]
